63eb9c55d7aa20db555596a2489abec35f66886f
root <root@centos-template.vm.local>
2022-07-07 01:10:24 +0300

    update ssh-key-copier script

--- ssh-key-copier/ssh-copier.sh
+++ ssh-key-copier/ssh-copier.sh
@@ -3,6 +3,7 @@
 hosts=$(cat ../inventory | awk '{ print $3}' | grep ansible_host | sed 's/ansible_host=//')
 
 for i in $hosts
-  do 
+  do
+     ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
      sshpass -f password.txt ssh-copy-id root@$i
 done
