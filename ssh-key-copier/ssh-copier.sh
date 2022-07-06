bc3bdf12025dec71e14edee5c7632926a6a22bc4
root <root@centos-template.vm.local>
2022-07-07 01:50:59 +0300

    improvements on gitlab role

--- ssh-key-copier/ssh-copier.sh
+++ ssh-key-copier/ssh-copier.sh
@@ -2,8 +2,9 @@
 
 hosts=$(cat ../inventory | awk '{ print $3}' | grep ansible_host | sed 's/ansible_host=//')
 
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
+
 for i in $hosts
   do
-     ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""
      sshpass -f password.txt ssh-copy-id root@$i
 done
